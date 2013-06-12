4e101816a95e192a5d022b8bbb34dac6a28605aa
singaripu <singlaripu@gmail.com>
2013-06-12 19:50:18 +0000

    upgraded flask sqlalchemy version

--- dbconnection.py
+++ dbconnection.py
@@ -1,8 +1,8 @@
-#from flask.ext.sqlalchemy import SQLAlchemy
-import sqlalchemy
+from flask.ext.sqlalchemy import SQLAlchemy
+#import sqlalchemy
 import os
 from flask import Flask
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
-db = sqlalchemy(app)
+db = SQLAlchemy(app)
